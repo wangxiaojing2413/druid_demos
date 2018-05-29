6640702a4a0f008d8f9931926a2ed8178f9c9c90
wangxiaojing2413 <18752031024@163.com>
2018-05-29 20:06:22 +0800

    druid web demos
    druid web demos

---
+++ druid_demo/src/main/java/com/whyun/util/db/source/AbstractDataSource.java
@@ -0,0 +1,13 @@
+package com.whyun.util.db.source;
+
+import javax.sql.DataSource;
+
+public abstract class AbstractDataSource {
+	protected DataSource dataSource;
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+	public  static AbstractDataSource getInstance() throws Exception {
+		return null;
+	}
+}
